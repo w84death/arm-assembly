f93d1af57622c6970146a4740e9104225c557832
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2018-01-23 19:05:00 +0100

    kinda working keyboard word tester 4 bits

--- tools/kbr_word_tester.asm
+++ tools/kbr_word_tester.asm
@@ -1,19 +1,24 @@
 .arm
 
 .data
+.balign 16
 input: 
-    .ascii "12345678123456781234567812345678"
+    .ascii " "
+.balign 16
 commands:
     .ascii "quit"
     .ascii "left"
     .ascii "right"
+    .ascii "back"
+    .ascii "forw"
+
 
 welcome: 
     .string "Welcome,\nyou can use <left>, <right> or <quit> to terminate the program. Have fun.\n"
 success: 
     .string "Success!\n"
 fail: 
-    .string "Fail...\n"
+    .string "...\n"
 quit: 
     .string "Bye!\n"
 
@@ -30,7 +35,7 @@ _start:
 _input:	
 	MOV R0, #0			@ keyboard
 	LDR R1, =input
-	MOV R2, #32	        @ length
+	MOV R2, #16	        @ length
 	MOV R7, #3			@ read
 	SWI 0
 	
@@ -39,18 +44,25 @@ _input:
 _testing:
     LDR R0, =input
 	LDR R0, [R0]
-	
-	LDR R1, =commands   
-	 
-	LDR R2, [R1]
+	LDR R1, =commands
+
+    LDR R2, [R1]
 	CMP R0, R2
 	BEQ _end
-	
+	 
 	LDR R2, [R1, #4]
 	CMP R0, R2
 	BEQ _success
-
+	
     LDR R2, [R1, #8]
+	CMP R0, R2
+	BEQ _success
+	
+	LDR R2, [R1, #12]
+	CMP R0, R2
+	BEQ _success
+	
+	LDR R2, [R1, #16]
 	CMP R0, R2
 	BEQ _success
 	
@@ -68,10 +80,10 @@ _success:
 _fail:
 	MOV R0, #1			@ keyboard
 	LDR R1, =fail
-	MOV R2, #9 			@ length
+	MOV R2, #4 			@ length
 	MOV R7, #4			@ write
 	SWI 0
-
+	
     b _input
     
 _end:
