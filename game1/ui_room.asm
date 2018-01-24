a3d11fadebc6a94a1b115dc38c2501964c328fdf
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2018-01-24 11:40:46 +0100

    game1 fixed ui_term

--- game1/ui_room.asm
+++ game1/ui_room.asm
@@ -1,9 +1,9 @@
 @ ---------------------------------------------------
 @
 @   Raspberry Pi Assembly GAME#1 by KJ/P1X
-@   
+@
 @   http://p1x.in | http://krzysztofjankowski.com
-@   https://github.com/w84death/arm-assembly        
+@   https://github.com/w84death/arm-assembly
 @
 @ ---------------------------------------------------
 .arm
@@ -14,25 +14,25 @@
 .global _ui_room
 
 _ui_room:
-    PUSH {R1-R4, LR}    
-    
+    PUSH {R1-R4, LR}
+
     MOV R1, #5 @0101
     BL _ui_term
-	
+
 	POP {R1-R2}
-	
+
 	MOV R0, #1
 	MOV R7, #4
 	SWI 0
-	
+
 	POP {R1-R2}
-	
+
 	MOV R0, #1
 	MOV R7, #4
 	SWI 0
-	
+
     MOV R1, #2 @0010
     BL _ui_term
-	
+
 	POP {LR}
 	BX LR
--- game1/ui_term.asm
+++ game1/ui_term.asm
@@ -1,9 +1,9 @@
 @ ---------------------------------------------------
 @
 @   Raspberry Pi Assembly GAME#1 by KJ/P1X
-@   
+@
 @   http://p1x.in | http://krzysztofjankowski.com
-@   https://github.com/w84death/arm-assembly        
+@   https://github.com/w84death/arm-assembly
 @
 @ ---------------------------------------------------
 .arm
@@ -13,7 +13,7 @@ clear_screen:
     .ascii "\033c"          @ 1 - clear screen
 clear_color:
     .ascii "\033[0m\000"    @ 2 - clear color
-color_red: 
+color_red:
     .ascii "\033[31m"       @ 4 - red
 color_green:
     .ascii "\033[31m"       @ 8 - green
@@ -21,8 +21,8 @@ color_blue:
     .ascii "\033[31m"       @ 16 - blue
 color_white:
     .ascii "\033[31m"       @ 32 - white
-    
-    
+
+
 .text
 .global _ui_term
 
@@ -31,40 +31,46 @@ _ui_term:
     AND R2, R1, #1
     CMP R2, #1
     BLEQ _clear_screen
-    
+
     AND R2, R1, #2
     CMP R2, #2
     BLEQ _clear_color
-    
+
     AND R2, R1, #4
     CMP R2, #4
     BLEQ _color_red
 
     POP {LR}
 	BX LR
-    
+
 _clear_screen:
-    MOV R0, #1    
+    PUSH {R1}
+    MOV R0, #1
     LDR R1, =clear_screen
     MOV R2, #2
     MOV R7, #4
     SWI 0
+    POP {R1}
     BX LR
-    
-_clear_color:    
-    MOV R0, #1    
+
+_clear_color:
+    PUSH {R1}
+    MOV R0, #1
     LDR R1, =clear_color
     MOV R2, #5
     MOV R7, #4
     SWI 0
+    POP {R1}
     BX LR
-    
-_color_red:    
-    MOV R0, #1    
+
+_color_red:
+    PUSH {R1}
+    MOV R0, #1
     LDR R1, =color_red
     MOV R2, #5
     MOV R7, #4
     SWI 0
+    POP {R1}
     BX LR
-	
-	
+
+
